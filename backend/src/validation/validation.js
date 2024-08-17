c62ef000264ccf95edcf6f4947fdceecc29325eb
Kevin Neel <87483788+KevinNeel@users.noreply.github.com>
2024-08-17 01:19:56 -0700

    Update validation.js

--- backend/src/validation/validation.js
+++ backend/src/validation/validation.js
@@ -7,21 +7,3 @@ export const validate_Register = [
     body('color').notEmpty().withMessage('Field cannot be empty'),
     body('skill').notEmpty().withMessage('Field cannot be empty'),
 ]
-
-export const validate_Login = [
-    body('email').isEmail().withMessage('Invalid Email Type'),
-    body('password').notEmpty().withMessage('Field cannot be empty')
-]
-
-export const validate_Password = [
-    body('password').isLength({ min: 6 }).withMessage('Min 6 length characters required'),
-    body('newPassword').isLength({ min: 6 }).withMessage('Min 6 length characters required')
-]
-
-export const validate_Product = [
-    body('prodcut_Name').notEmpty().withMessage('Field cannot be empty'),
-    body('description').notEmpty().withMessage('Field cannot be empty'),
-    body('quantity').isNumeric().withMessage('Only Decimals allowed'),
-    body('price').isNumeric().withMessage('Only Decimals allowed'),
-    body('category').notEmpty().withMessage('Field cannot be empty'),
-]
